9c3bf2b69854afd7accad1e40d6b2ab780c77e64
Daniel dos Santos <daniccxp19@gmail.com>
2023-09-21 13:41:19 -0300

    barra menu

--- 2MENU_RESPONSIVO/1_mobile/src/componentes/Header.jsx
+++ 2MENU_RESPONSIVO/1_mobile/src/componentes/Header.jsx
@@ -1,20 +1,35 @@
 import{List } from "@phosphor-icons/react"
 import styles from "./Header.module.css"
+import { useState } from "react"
 
 function Header() {
+
+const[Abrirmenu, setAbrirmenu] = useState(false)
+
+function handleAbrirFechar(){
+if (Abrirmenu === true) {
+setAbrirmenu(false)
+return
+}
+
+setAbrirmenu(true)
+
+}
+
+
   return (
     <header className={styles.header}>
 <div className={styles.mobile}>
     <div className={styles.menu}>
-        <button>
+        <button onClick={handleAbrirFechar} >
         <List size={32} />
-        </button>
+        </button >
 
         <h3>Logo</h3>
     
     </div>
-    <div className={styles.options}>
-      <nav>
+    <div className={`${styles.options} ${Abrirmenu === true && styles.open}`}>
+      <nav className="">
         <a href="">Home</a>
         <a href="">Resultados</a>
         <a href="">Produtos</a>
